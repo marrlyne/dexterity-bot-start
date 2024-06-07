56ca109a341e5377e22cf91e6d3b4b28af5b8249
Marrlyne <marrlyne63@gmail.com>
2024-06-08 00:48:56 +0100

    Update newAccountHandler.ts

--- src/api-utils/newAccountHandler.ts
+++ src/api-utils/newAccountHandler.ts
@@ -1,6 +1,27 @@
-const HeliusApiKey = "";
-const WebHookId = "";
+const HeliusApiKey = process.env.APP_API_KEY;
+const WebHookId = process.env.APP_WEBHOOK;
 
 export const newAcccountSubscriptionHandler = async (newTrg: string) => {
-
+    try {
+        const response = await fetch(`https://api.helius.xyz/v0/webhooks/${WebHookId}?api-key=${HeliusApiKey}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              webhookURL: "https://trades-webhook-url.com/webhook",
+              transactionTypes: ["Any"],
+              accountAddresses: [newTrg],
+              webhookType: "rawDevnet",
+            }),
+          }
+        );
+      
+        const data = await response.json();
+        console.log({ data });
+      
+      } catch (e) {
+        console.error("error", e);
+      }
 };
